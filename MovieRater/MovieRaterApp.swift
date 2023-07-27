311bad0f38aed583dc6c99bc96af7a641d0dfe16
noelpettersson <90799107+noelpettersson@users.noreply.github.com>
2023-07-27 19:01:27 +0200

    Initial Commit

---
+++ MovieRater/MovieRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRaterApp.swift
+//  MovieRater
+//
+//  Created by Anders Pettersson on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct MovieRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
